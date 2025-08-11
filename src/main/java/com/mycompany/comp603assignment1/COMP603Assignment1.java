55e955f31652d6af9aa381e2f26e3377285f24b7
JesseKilkelly <vhw3478@autuni.ac.nz>
2025-08-11 13:23:45 +1200

    Update COMP603Assignment1.java
    Added more to the to do list

--- src/main/java/com/mycompany/comp603assignment1/COMP603Assignment1.java
+++ src/main/java/com/mycompany/comp603assignment1/COMP603Assignment1.java
@@ -4,12 +4,15 @@ package com.mycompany.comp603assignment1;
  * Todo:
  * Lifelines - implement - use Random().nextInt(x) to decide if correct or not
  * using a lifeline removes it from being used
- * Correct the scoring
+ * --Correct the scoring-- done
  * When loading player, make their currentLevel be the question they are up to
  * reset the level after losing or passing the million dollar question
+ * Check input for only A B C or D
+ * If the player has already beaten the game, check if they would like to play again (Overriding their current level or adding the score on top)
  * 
  * Bonus:
  * more questions and randomize them
+ * Do a 'Are you sure? is that your final answer?' check
  * @author Jesse
  */
 public class COMP603Assignment1 {

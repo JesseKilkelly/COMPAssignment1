c87a98c1137d2cbc625dfa12280a8e3864694498
JesseKilkelly <vhw3478@autuni.ac.nz>
2025-08-12 16:18:14 +1200

    Update Game.java
    / Moved some things to the cui class
    / Attempting to make lifelines work

--- src/main/java/com/mycompany/comp603assignment1/Game.java
+++ src/main/java/com/mycompany/comp603assignment1/Game.java
@@ -22,15 +22,8 @@ public class Game {
         this.loadGame = new LoadGame();
         
         //Load
-        //easy questions 5
-        this.easy = new QuestionBank();
-        this.easy.loadFile("./resources/easy.txt");
-        //hard questions 9
-        this.hard = new QuestionBank();
-        this.hard.loadFile("./resources/hard.txt");
-        //final question 1
-        this.finalQuestion = new QuestionBank();
-        this.finalQuestion.loadFile("./resources/final.txt");
+        qList = new QuestionBank();
+        qList.loadAllQuestions("./resources/easy.txt", "./resources/hard.txt", "./resources/final.txt");
         
         //multi initialise - polymorphism
         this.lifelines = new LifeLine[]{new FiftyFifty(), new PhoneAFriend(), new AudiencePoll()};
@@ -55,47 +48,48 @@ public class Game {
             System.out.println("Welcome newcomer, " + name);
         }
         
-        //start at easy ------ change this to check player level
-        QuestionBank currentQuestions = easy;
-        
         //Game loop
         while(true) {
-            Question q = currentQuestions.getNextQuestion();
+            Question currentQuestion = qList.getQuestionByLevel(player.getLevel());
             
-            //loop through the questions
-            if(q == null){
-                if(currentQuestions == easy){
-                    currentQuestions = hard;
-                    continue;
-                } else if (currentQuestions == hard){
-                    currentQuestions = finalQuestion;
-                    continue;
-                }else{
-                    System.out.println("Congratulations! youre now a Millionaire!");
-                    saveGame.save(player);
-                    break;
-                }
+            //got beyond the question list
+            if (currentQuestion == null){
+                cui.congrats();
+                saveGame.save(player);
+                break;
             }
-            
+
             //display question
             cui.displayQuestion(q);
             int answer = cui.getAnswer();
             
             //check answer
-            if(q.IsCorrect(answer)){
+            //They got it right - level up give score.
+            if(currentQuestion.IsCorrect(answer)){
                 cui.displayCorrect();
                 player.levelUp();
+                
                 //get score from array and set it
                 if (player.getLevel() <= priseArray.length){
                     int prise = priseArray[player.getLevel()-1];
                     player.setScore(prise);
                 }
-                System.out.println("Your current score is now: $" + player.getScore());
+                cui.displayScore(player.getScore());
                 //make sure the data is up to date
                 saveGame.save(player);
-            } else{
-                cui.displayIncorrect(q);
-                System.out.println("Game over. Your final score was: $" + player.getScore());
+            }
+            else if(cui.getAnswer() == 1)//&& phone a friend hasnt been used)
+            {
+                //phone a friend has been used
+            }
+            //They got it wrong - reset player, kick them out
+            else{
+                cui.displayIncorrect(currentQuestion);
+                cui.displayFinalScore(player.getScore());
+                player.setLevel(0);
+                //check if safety net here
+                player.setScore(0);
+                saveGame.save(player);
                 break;
             }
             //If quits

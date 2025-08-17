50cc1441185c9e79d33e3b5c8b21cfaaf53ff116
JesseKilkelly <vhw3478@autuni.ac.nz>
2025-08-18 11:08:57 +1200

    Update FiftyFifty.java
    +this shows the two options
    /but then game displays the whole question which can be fixed at somepoint

--- src/main/java/com/mycompany/comp603assignment1/FiftyFifty.java
+++ src/main/java/com/mycompany/comp603assignment1/FiftyFifty.java
@@ -1,16 +1,35 @@
 package com.mycompany.comp603assignment1;
 
 /**
- * life line to removes 2 wrong options
+ * give a 50:50 hint 
  * @author Jesse
  */
 public class FiftyFifty extends LifeLine{
+    private final Random rand = new Random();
+    
     public FiftyFifty(){
         super("50:50");
     }
     
     public void use(Question q){
-        //Will recieve the question from game get the correct answer and 
-        //remove 2 of the wrong answers
+        List<String> options = q.getOptions();
+        int answerIndex = q.getCorrectIndex();
+        
+        //get the wrong answers into a list
+        List<Integer> wrong = new ArrayList<>();
+        for (int i = 0; i < options.size(); ++i){
+            if(i != answerIndex)
+                wrong.add(i);
+        }
+        Collections.shuffle(wrong, rand);
+        int wrongIndex = wrong.get(0);
+        
+        //convert index to option characters
+        char firstLetter = (char)('A' + answerIndex);
+        char secondLetter = (char)('A' + wrongIndex);
+        
+        System.out.println("Reducing the answer to two possible answers we have");
+        System.out.println("(" +firstLetter+ ") " +options.get(answerIndex)+ " or");
+        System.out.println("(" +secondLetter+ ") " +options.get(wrongIndex));   
     }
 }

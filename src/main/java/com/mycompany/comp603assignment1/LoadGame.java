33c3752cead6677a6747b6ddcd586c2e67bb261d
JesseKilkelly <vhw3478@autuni.ac.nz>
2025-08-23 17:29:54 +1200

    Update LoadGame.java

--- src/main/java/com/mycompany/comp603assignment1/LoadGame.java
+++ src/main/java/com/mycompany/comp603assignment1/LoadGame.java
@@ -4,8 +4,7 @@ import java.io.*;
 /**
  * used to check player data from previous saves
  * Todo:
- * not sure how this would handle loading a bunch of different players
- *              might need to loop through or something
+ * make this check through multiple players in a list
  * @author Jesse
  */
 public class LoadGame {
@@ -13,11 +12,17 @@ public class LoadGame {
     public Player load()
     {
         try(BufferedReader reader = new BufferedReader(new FileReader("./resources/WWTBAMsave.txt"))){
+            //load the individual parts
             String name = reader.readLine();
             int level = Integer.parseInt(reader.readLine());
             int score = Integer.parseInt(reader.readLine());
+            boolean[] lifelines = new boolean[3];
+            for (int i = 0; i < 3; ++i){
+                lifelines[i] = Boolean.parseBoolean(reader.readLine());
+            }
             
-            Player player = new Player(name, level, score);
+            //then create new player using loaded parts
+            Player player = new Player(name, level, score, lifelines);
             return player;
         }
         catch(IOException | NumberFormatException ex){

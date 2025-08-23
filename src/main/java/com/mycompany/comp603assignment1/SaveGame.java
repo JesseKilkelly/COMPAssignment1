b33a2da71903b3ed8e0422a3188d051aa9fa4137
JesseKilkelly <vhw3478@autuni.ac.nz>
2025-08-23 17:38:57 +1200

    Update SaveGame.java

--- src/main/java/com/mycompany/comp603assignment1/SaveGame.java
+++ src/main/java/com/mycompany/comp603assignment1/SaveGame.java
@@ -3,15 +3,19 @@ package com.mycompany.comp603assignment1;
 import java.io.*;
 /**
  * saves player data to a file to be read from
+ * Here is the at least 3 save files
  * @author Jesse
  */
 public class SaveGame {
     public void save(Player player){
-        //write to file (true to append saves?
-        try(PrintWriter writer = new PrintWriter(new FileWriter("./resources/WWTBAMsave.txt",true))){
+        //write to file (true to append saves?)
+        try(PrintWriter writer = new PrintWriter(new FileWriter("./resources/WWTBAMsave.txt"))){
             writer.println(player.getName());
             writer.println(player.getLevel());
             writer.println(player.getScore());
+            for (int i = 0; i < 3; ++i){
+                writer.println(player.hasLifeline(i));
+            }
         }catch(IOException ex){
             System.out.println("Error Saving: " +ex.getMessage());
         }

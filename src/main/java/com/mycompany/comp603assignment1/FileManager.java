aeff94b5b398e7f8c05b92453898fac757bb6dd3
JesseKilkelly <vhw3478@autuni.ac.nz>
2025-08-23 17:28:11 +1200

    Update FileManager.java

--- src/main/java/com/mycompany/comp603assignment1/FileManager.java
+++ src/main/java/com/mycompany/comp603assignment1/FileManager.java
@@ -4,10 +4,11 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Core class for handling all the reading and writing
+ * Was initially for saving and loading but now handles just question bank
  * @author Jesse
  */
 public class FileManager {
+    
     public List<String> readFile(String path){
         List<String> lines = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader(path))){
@@ -20,12 +21,4 @@ public class FileManager {
         }
         return lines;
     }
-    
-    public void writeToFile(String path, String data, boolean append){
-        try(PrintWriter writer = new PrintWriter(new FileWriter(path, append))){
-            writer.println(data);
-        }catch(IOException ex){
-            System.out.println("Error writing " + ex.getMessage());
-        }
-    }   
 }
